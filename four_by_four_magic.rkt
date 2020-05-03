b835c7ab9f41c77474dae436162b769128412547
Pavlo Lyalyutskyy <pavlo_lyalyutskyy@brown.edu>
2020-05-03 18:49:23 -0400

    Collaborators and Four by Four Magic Square Instance Tests

--- four_by_four_magic.rkt
+++ four_by_four_magic.rkt
@@ -2,7 +2,8 @@
 
 --represents Coord * Coord board of Int
 sig Board {
-  places: set Int -> Int -> Int --row->col->N value
+  places: set Int -> Int -> Int, --row->col->N value
+  filled_places: set Int -> Int -> Int
 }
 
 pred structural[b: Board] {
@@ -32,11 +33,58 @@ pred structural[b: Board] {
         b.places[sing[1]][sing[2]]+
         b.places[sing[2]][sing[1]]+
         b.places[sing[3]][sing[0]]] = sum[sing[34]]
+
+    b.filled_places in b.places
 }
 
 --creates a 4x4 board
-run {
-    all b: Board {
-        structural[b]       
-    }
-} for exactly 1 Board, 6 Int 
+--run {
+--    all b: Board {
+--        structural[b]       
+--    }
+--} for exactly 1 Board, 6 Int
+
+inst existence {
+    -- show that there exists solutions to 4x4
+    Int[6]
+    Board = Board0
+    --places = Board0->sing[1]->sing[1]->sing[2]
+}
+
+inst verify_correct_solution {
+    -- verify a correct solution
+    Int[6]
+    Board = Board0
+    places = Board0->sing[0]->sing[0]->sing[8]
+    + Board0->sing[0]->sing[1]->sing[1]
+    + Board0->sing[0]->sing[2]->sing[11]
+    + Board0->sing[0]->sing[3]->sing[14]
+    + Board0->sing[1]->sing[0]->sing[10]
+    + Board0->sing[1]->sing[1]->sing[15]
+    + Board0->sing[1]->sing[2]->sing[5]
+    + Board0->sing[1]->sing[3]->sing[4]
+    + Board0->sing[2]->sing[0]->sing[13]
+    + Board0->sing[2]->sing[1]->sing[12]
+    + Board0->sing[2]->sing[2]->sing[2]
+    + Board0->sing[2]->sing[3]->sing[7]
+    + Board0->sing[3]->sing[0]->sing[3]
+    + Board0->sing[3]->sing[1]->sing[6]
+    + Board0->sing[3]->sing[2]->sing[16]
+    + Board0->sing[3]->sing[3]->sing[9]
+}
+
+inst one_value_there {
+    -- tests that a solution would be found while
+    -- only having one value filled in
+    Int[6]
+    Board = Board0
+    filled_places = Board0->sing[1]->sing[1]->sing[2]
+}
+
+
+
+test expect {
+  { structural[Board] } for existence is sat
+  { structural[Board] } for verify_correct_solution is sat
+  { structural[Board] } for one_value_there is sat
+}
